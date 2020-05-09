80996e2388dfd017c816f5ac9c9f547d63b4065f
Kim Covaci <kim.covaci@soundcloud.com>
2020-05-09 10:03:05 +0200

    EitherT ErrorMonad

--- src/main/scala/instances/errorMonads.scala
+++ src/main/scala/instances/errorMonads.scala
@@ -4,47 +4,33 @@ import classes._
 import data._
 import com.twitter.util.Future
 
-given ErrorMonad[Either] {
+def eithetTErrorMonad[F[+_]](using m: Monad[F]) = new ErrorMonad[[L, R] =>> EitherT[F, L, R]] {
 
-  override def pure[L, R](a: R): Either[L, R] = Right(a)
-
-  override def [L, LL, R, RR](either: Either[L, R])
-      flatMap(f: R => Either[LL, RR]): Either[L | LL, RR] = {
-
-    either match {
-      case Right(v) => f(v)
-      case Left(e) => Left(e)
-    }
+  override def pure[L, R](a: R): EitherT[F, L, R] = {
+    val e: Either[L, R] = Right(a)
+    EitherT(m.pure(e))
   }
 
-  override def raiseError[E, A](e: E): Either[E, A] = Left(e)
-
-  override def [E, A](fa: Either[E, A]) handleErrors(f: E => A): Either[Nothing, A] = {
-    fa match {
-      case Right(v) => Right(v)
-      case Left(e) => Right(f(e))
-    }
-  }
-}
-
-given (using m: Monad[Future]) as ErrorMonad[Outcome] {
-
-  override def pure[L, R](a: R): Outcome[L, R] = Outcome.value(a)
-
-  override def [L, LL, R, RR](outcome: Outcome[L, R])
-      flatMap(f: R => Outcome[LL, RR]): Outcome[L | LL, RR] = {
-
-    val either = m.flatMap(outcome.value) {
+  override def [L, LL, R, RR](eitherT: EitherT[F, L, R])
+     flatMap(f: R => EitherT[F, LL, RR]): EitherT[F, L | LL, RR] = {
+    val either = m.flatMap(eitherT.value) {
       case Right(v) => f(v).value
       case Left(e) => m.pure(Left(e))
     }
     EitherT(either)
   }
 
-  override def raiseError[E, A](e: E): Outcome[E, A] = Outcome.error(e)
+  def raiseError[E, A](e: E): EitherT[F, E, A] = {
+    EitherT(m.pure(Left(e)))
+  }
 
-  override def [E, A](fa: Outcome[E, A]) handleErrors(f: E => A): Outcome[Nothing, A] = {
-    val either: Future[Either[Nothing, A]] = fa.value.map(_.handleErrors(f))
-    EitherT(either)
+  override def [E, A](fa: EitherT[F, E, A]) handleErrors(f: E => A): EitherT[F, Nothing, A] = {
+    val eitherF: F[Either[Nothing, A]] = fa.value.map {
+      case Right(a) => Right(a)
+      case Left(e) => Right(f(e))
+    }
+    EitherT(eitherF)
   }
 }
+
+given outcomeErrorMonad as ErrorMonad[Outcome] = eithetTErrorMonad
